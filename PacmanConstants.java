43e8de337c9d459eccc55cec08f0f37805cfc5c5
lechengfan <tommy9573@hotmail.com>
2013-12-29 01:05:04 -0700

    Create PacmanConstants.java

---
+++ PacmanConstants.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+
+public interface PacmanConstants {
+	/**Constants*/
+	public static final int APPLICATION_WIDTH = 570;
+	public static final int APPLICATION_HEIGHT = 630;
+
+	public static final int WIDTH = APPLICATION_WIDTH;
+	public static final int HEIGHT = APPLICATION_HEIGHT;
+	public static final int NROWS=21;
+	public static final int NCOLS=19;
+	public static final Color BACKGROUND_COLOR=Color.black;
+	
+	public static final int SMALL_DOT_SIZE=10;
+	public static final int WALL_SIZE=30;
+	public static final int PACMAN_SIZE = 28;
+	public static final double EPSILON = 1e-10;
+	public static final int DELAY = 7;
+	public static final double SPEED = 1;
+	
+}
