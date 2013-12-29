40b794cb5b159cb353970f4dd081650a9ed0cdd2
lechengfan <tommy9573@hotmail.com>
2013-12-28 23:32:18 -0700

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -16,6 +16,7 @@ public class Pacman extends GraphicsProgram {
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 	private static final int NROWS=21;
 	private static final int NCOLS=19;
+	private static final Color BACKGROUND_COLOR=Color.black;
 	
 	private static final int SMALL_DOT_SIZE=10;
 	private static final int WALL_SIZE=30;
@@ -24,14 +25,11 @@ public class Pacman extends GraphicsProgram {
 	private static final int DELAY = 7;
 	private static final double SPEED = 1;
 	
-	
-	
 	/**Private instance variables*/
-	
 	private double vx=SPEED;
 	private double vy=0;
-	private GPacman pacman;
-	private int turnRequest=0;
+	private GPacman pacman1;
+	private GPacman pacman2;
 	
 	public void init() {
 		setUpGameboard();
@@ -40,11 +38,11 @@ public class Pacman extends GraphicsProgram {
 		startGame();
 		
 		while(true){
-		
 			move();
 			changeDirection();
 			adjustForCollision();
 			eatDots();
+			teleport();
 			pause(DELAY);
 		}
 		
@@ -52,24 +50,34 @@ public class Pacman extends GraphicsProgram {
 	private void adjustForCollision() {
 		if(getObjectinFront() instanceof GRect) {
 			if(vx>0) {
-				pacman.move(-1, 0);
+				pacman1.move(-1, 0);
 			}
 			else if(vx<0) {
-				pacman.move(1, 0);
+				pacman1.move(1, 0);
 			}
 			else if(vy>0) {
-				pacman.move(0, -1);
+				pacman1.move(0, -1);
 			}
 			else if(vy<0) {
-				pacman.move(0, 1);
+				pacman1.move(0, 1);
 			}
 		}
 	}
+	
 	private void setUpGameboard() {
-		setBackground(Color.black);
-		pacman=new GPacman(PACMAN_SIZE);
-		add(pacman,WALL_SIZE+1,WALL_SIZE+1);
+		setBackground(BACKGROUND_COLOR);
+		initiatePacman();
 		addWalls();
+	}
+	
+	private void initiatePacman() {
+		pacman1=new GPacman(PACMAN_SIZE);
+		add(pacman1,WALL_SIZE+1,WALL_SIZE+1);
+		pacman2=new GPacman(PACMAN_SIZE);
+		pacman2.setBodyColor(new Color(157,74,0));
+		pacman2.turnLeft();
+		add(pacman2,WIDTH-2*WALL_SIZE+1,HEIGHT-2*WALL_SIZE+1);
+		
 	}
 	private void addWalls() {
 		boolean[][] wallsArray={{true, true, true, true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true},
@@ -122,6 +130,7 @@ public class Pacman extends GraphicsProgram {
 		add(wall,xcoord,ycoord);
 	}
 	private void changeDirection() {
+		int turnRequest=pacman1.getTurnRequest();
 		if(turnRequest==1&&canGoVertical()&&topIsClear()) {
 			vx=0;
 			vy=-1;
@@ -146,18 +155,18 @@ public class Pacman extends GraphicsProgram {
 	}
 	private void move() {
 		if(vy<0) {
-			pacman.turnUp();
+			pacman1.turnUp();
 		}
 		else if(vy>0) {
-			pacman.turnDown();
+			pacman1.turnDown();
 		}
 		else if(vx>0) {
-			pacman.turnRight();
+			pacman1.turnRight();
 		}
 		else if(vx<0) {
-			pacman.turnLeft();
+			pacman1.turnLeft();
 		}
-		pacman.move(vx, vy);
+		pacman1.move(vx, vy);
 		
 	}
 	private void startGame() {
@@ -174,21 +183,9 @@ public class Pacman extends GraphicsProgram {
 			remove(collision);
 		}
 	}
-//	private GObject getObjectAtCorners() {
-//		GObject object = getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()-1);	//Top
-//		if(object == null) {
-//			object = getElementAt(pacman.getX()-1, pacman.getY()+PACMAN_SIZE/2);	//Left
-//			if(object == null) {
-//				object = getElementAt(pacman.getX()+PACMAN_SIZE+1, pacman.getY()+PACMAN_SIZE/2);	//Right
-//				if(object==null) {
-//					object = getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()+PACMAN_SIZE+1);	//Bottom
-//				}
-//			}
-//		}
-//		return object;
-//	}
+
 	private boolean canGoHorizontal() {
-		double ycoord= pacman.getY()+PACMAN_SIZE/2;
+		double ycoord= pacman1.getY()+PACMAN_SIZE/2;
 		for(int i=0;i<NROWS;i++) {
 			if((15+i*30-1)<ycoord&&ycoord<(15+i*30+1)) {
 				return true;
@@ -197,7 +194,7 @@ public class Pacman extends GraphicsProgram {
 		return false;
 	}
 	private boolean canGoVertical() {
-		double xcoord= pacman.getX()+PACMAN_SIZE/2;
+		double xcoord= pacman1.getX()+PACMAN_SIZE/2;
 		for(int i=0;i<NCOLS;i++) {
 			if((15+i*30-1)<=xcoord&&xcoord<=(15+i*30+1)) {
 				return true;
@@ -206,43 +203,57 @@ public class Pacman extends GraphicsProgram {
 		return false;
 	}
 	private boolean topIsClear() {
-        return getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()-3)==null;
+        return getElementAt(pacman1.getX()+PACMAN_SIZE/2, pacman1.getY()-3)==null;
 	}
 	private boolean bottomIsClear() {
-        return getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()+PACMAN_SIZE+3)==null;
+        return getElementAt(pacman1.getX()+PACMAN_SIZE/2, pacman1.getY()+PACMAN_SIZE+3)==null;
 	}
 	private boolean	leftIsClear() {
-        return getElementAt(pacman.getX()-3, pacman.getY()+PACMAN_SIZE/2)==null;
+        return getElementAt(pacman1.getX()-3, pacman1.getY()+PACMAN_SIZE/2)==null;
 	}
 	private boolean rightIsClear() {
-        return getElementAt(pacman.getX()+PACMAN_SIZE+3, pacman.getY()+PACMAN_SIZE/2)==null;
+        return getElementAt(pacman1.getX()+PACMAN_SIZE+3, pacman1.getY()+PACMAN_SIZE/2)==null;
 	}
 	private GObject getObjectinFront() {
 		if(vx>0) {
-			return getElementAt(pacman.getX()+PACMAN_SIZE+1, pacman.getY()+PACMAN_SIZE/2);
+			return getElementAt(pacman1.getX()+PACMAN_SIZE+1, pacman1.getY()+PACMAN_SIZE/2);
 		}
 		else if(vx<0) {
-			return getElementAt(pacman.getX()-1, pacman.getY()+PACMAN_SIZE/2);
+			return getElementAt(pacman1.getX()-1, pacman1.getY()+PACMAN_SIZE/2);
 		}
 		else if(vy>0) {
-			return getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()+PACMAN_SIZE+1);
+			return getElementAt(pacman1.getX()+PACMAN_SIZE/2, pacman1.getY()+PACMAN_SIZE+1);
 		}
 		else if(vy<0) {
-			return getElementAt(pacman.getX()+PACMAN_SIZE/2, pacman.getY()-1);
+			return getElementAt(pacman1.getX()+PACMAN_SIZE/2, pacman1.getY()-1);
 		}
 		return null;
-	
+	}
+	/**
+	 * Method: teleport()
+	 * 
+	 * When Pacman travels off-screen either to the left or right, this method teleports Pacman to the other side of 
+	 * the screen. 
+	 */
+	private void teleport() {
+		if(pacman1.getX()+PACMAN_SIZE/2<0) {
+			pacman1.setLocation(WIDTH-PACMAN_SIZE/2, pacman1.getY());
+		}
+		else if(pacman1.getX()+PACMAN_SIZE/2>WIDTH) {
+			pacman1.setLocation(-PACMAN_SIZE/2, pacman1.getY());
+		}
 	}
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()) {
-			case KeyEvent.VK_UP:	turnRequest=1; break;
-			case KeyEvent.VK_DOWN:	turnRequest=2; break;
-			case KeyEvent.VK_LEFT:	turnRequest=3; break;
-			case KeyEvent.VK_RIGHT:	turnRequest=4; break;
+			case KeyEvent.VK_UP:	pacman1.setTurnRequest(1); break;
+			case KeyEvent.VK_DOWN:	pacman1.setTurnRequest(2); break;
+			case KeyEvent.VK_LEFT:	pacman1.setTurnRequest(3); break;
+			case KeyEvent.VK_RIGHT:	pacman1.setTurnRequest(4); break;
 		}
 	}
+	
 //	private boolean inBoundaries() {
-//		return pacman.getX()>0&&(pacman.getX()+PACMAN_SIZE)<WIDTH&&pacman.getY()>0&&(pacman.getY()+PACMAN_SIZE)<HEIGHT;
+//		return pacman1.getX()>0&&(pacman1.getX()+PACMAN_SIZE)<WIDTH&&pacman1.getY()>0&&(pacman1.getY()+PACMAN_SIZE)<HEIGHT;
 //	}
 
 }
