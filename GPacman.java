eb2e8c551b826c9f33b2f16e4e37beb12b164685
lechengfan <tommy9573@hotmail.com>
2013-12-30 00:13:23 -0700

    Update GPacman.java

--- GPacman.java
+++ GPacman.java
@@ -3,32 +3,23 @@ import java.awt.Color;
 import acm.graphics.*;
 
 
-public class GPacman extends GCompound{ 
+public class GPacman extends PacmanCharacter{ 
 	
 	/** Private instance variables*/
-	//Diameter of the Pacman's body
-	private double size;
+
 	private GArc body;
 	//Says what frame the Pacman animation is on goes from 0-15 inclusive 
     private int frameCounter;
-    
-    private int direction;
     //Says whether frame number is increasing or decreasing
     private boolean increasing;
-    //Variable that tells you which direction you want to turn. 1=Up 2=Down 3=Left 4=Right
-	private int turnRequest=0;	
-	//Color of the Pacman
-	private Color bodyColor=Color.yellow;
-	//x Velocity
-	private double vx;
-	//y Velocity
-	private double vy;
 	
-	public GPacman(double diameter) {
+	public GPacman(double diameter, Color color) {
+		super(color);
         increasing = true;
         frameCounter = 0;
         direction = 4;
         size= diameter;
+
         body = new GArc(size, size, 45 - frameCounter * 3, 270 + frameCounter * 6);
         body.setFilled(true);
         body.setColor(bodyColor);
@@ -107,45 +98,5 @@ public class GPacman extends GCompound{
 	        body.setColor(bodyColor);
 	        add(body);
 	}
-	
-	/**
-	 * 
-	 * @param n The turn request that you want to set.
-	 */
-	public void setTurnRequest(int n) {
-		turnRequest=n;
-	}
-	/**
-	 * 
-	 * @return Returns turnRequest
-	 */
-	public int getTurnRequest() {
-		return turnRequest;
-	}
-	/**
-	 * Sets the color of Pacman's body.
-	 * @param c The new color
-	 */
-	public void setBodyColor(Color c) {
-		bodyColor=c;
-	}
-	/**
-	 * Sets vx and vy according to the parameters.
-	 * @param x New x velocity
-	 * @param y New y velocity
-	 */
-	public void setVelocities(double x, double y) {
-		vx=x;
-		vy=y;
-	}
-	public double getXVelocity() {
-		return vx;
-	}
-	public double getYVelocity() {
-		return vy;
-	}
-	public int getDirection() {
-		return direction;
-	}
-	
+
 }
