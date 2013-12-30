aaa49ad881144a6e5fb5348c80c0f2405000bc04
lechengfan <tommy9573@hotmail.com>
2013-12-30 00:08:50 -0700

    Update Ghost.java

--- Ghost.java
+++ Ghost.java
@@ -1,70 +1,47 @@
-import java.awt.Color;
-import acm.graphics.*;
+	import java.awt.Color;
+	import acm.graphics.*;
 
-public abstract class Ghost extends GCompound{
+	public abstract class Ghost extends PacmanCharacter{
 
-	private double vx;
-	private double vy;
-	private GArc body;
-	private Color color;
-	//1 is right, 2 is up, 3 is left, and 4 is down
-	private int nextDirection;
-	
-	public Ghost(){
-		vx = -PacmanConstants.SPEED;
-		vy = 0;
-		setNextDirection(1);
-	}
-	
-	public void decideNextDirection(GPacman[] targets){	
-	
-	}
-	
-	
-	public void setVelocities(double x, double y) {
-        vx=x;
-        vy=y;
-	}
-	
-	public double getVx(){
-		return vx;
-	}
-	
-	public void setVx(double input){
-		vx = input;
-	}
-	
-	public void setVy(double input){
-		vy = input;
-	}
-	
-	public double getVy(){
-		return vy;
-	}
+		
+		private GArc body;
+		//1 is right, 2 is up, 3 is left, and 4 is down
+		private int nextDirection;
+		
+		public Ghost(Color color){
+			super(color);
+			vx = -PacmanConstants.SPEED;
+			vy = 0;
+			setNextDirection(1);
+		}
+		
+		abstract void decideNextDirection(GPacman[] targets);
+		
+		
+		public int getNextDirection() {
+			return nextDirection;
+		}
 
-	public int getNextDirection() {
-		return nextDirection;
+		public void setNextDirection(int nextDirection) {
+			this.nextDirection = nextDirection;
+		}
+		
+		public Color getColor(){
+			return bodyColor;
+		}
+		
+		public void setColor(Color color){
+			bodyColor = color;
+		}
+		
+		public GArc getBody(){
+			return body;
+		}
+		
+		public void setBody(GArc body){
+			this.body = body;
+		}
+		
 	}
 
-	public void setNextDirection(int nextDirection) {
-		this.nextDirection = nextDirection;
-	}
-	
-	public Color getColor(){
-		return color;
-	}
-	
-	public void setColor(Color color){
-		this.color = color;
-	}
-	
-	public GArc getBody(){
-		return body;
-	}
-	
-	public void setBody(GArc body){
-		this.body = body;
-	}
-	
-}
 
