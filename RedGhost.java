d4a39c85e4fbd1e8cc2b840abee8b7db4fb9004d
Ian <ianchar4@gmail.com>
2014-01-03 17:08:01 -0700

    RedGhost now works
    although the code works now, the algorithm for decideNextDirection still needs to be altered

--- RedGhost.java
+++ RedGhost.java
@@ -3,47 +3,46 @@ import acm.graphics.*;
 
 public class RedGhost extends Ghost implements PacmanConstants{
 
-	public RedGhost(Color color){
-		super(color);
-		setVelocities(-SPEED,0);
-		setNextDirection(1);
-		setBody( new GArc(28,28,0,360));
-		getBody().setFilled(true);
-		getBody().setColor(bodyColor);
-		add(getBody());
-	}
-	
-	public void decideNextDirection(GPacman[] targets){	
-		double temp = Math.abs(targets[0].getX() - getX());
-		int tempDir = 1;
-		
-		for(GPacman target : targets)
-		{
-			double yDif = target.getY()-getY();
-			double xDif = target.getX()-getX();
-			
-			if(Math.abs(xDif) < temp && Math.abs(xDif) > 0)
-			{
-				temp = Math.abs(xDif);
-				if(xDif > 0)
-					tempDir = 1;
-				else
-					tempDir = 3;
-			}
-			
-			if(Math.abs(yDif) < temp && Math.abs(yDif) > 0)
-			{
-				temp = Math.abs(yDif);
-				if(yDif > 0)
-					tempDir = 1;
-				else
-					tempDir = 3;
-			}
-		}
-		
-		setNextDirection(tempDir);
-	}
-	public void animate() {
-		
-	}
+        public RedGhost(Color color){
+                super(color);
+                setVelocities(-SPEED,0);
+                setBody( new GArc(28,28,0,360));
+                getBody().setFilled(true);
+                getBody().setColor(bodyColor);
+                add(getBody());
+        }
+        
+        public void decideNextDirection(GPacman[] targets){        
+                double temp = 100000;
+                int tempDir = 1;
+                
+                for(GPacman target : targets)
+                {
+                        double yDif = target.getY()-getY();
+                        double xDif = target.getX()-getX();
+                        
+                        if(Math.abs(xDif) <= temp )//&& Math.abs(xDif) > 30)
+                        {
+                                temp = Math.abs(xDif);
+                                if(xDif > 0)
+                                        tempDir = 1;
+                                else
+                                        tempDir = 3;
+                        }
+                        
+                        if(Math.abs(yDif) < temp)// && Math.abs(yDif) > 30)
+                        {
+                                temp = Math.abs(yDif);
+                                if(yDif > 0)
+                                        tempDir = 4;
+                                else
+                                        tempDir = 2;
+                        }
+                }
+                
+                setTurnRequest(tempDir);
+        }
+        public void animate() {
+                
+        }
 }
