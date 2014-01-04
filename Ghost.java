ae90b2663588a10bb94c88f4a246bda584643944
Ian <ianchar4@gmail.com>
2014-01-03 17:08:38 -0700

    Got rid of not needed code

--- Ghost.java
+++ Ghost.java
@@ -1,47 +1,37 @@
 	import java.awt.Color;
-	import acm.graphics.*;
+        import acm.graphics.*;
 
-	public abstract class Ghost extends PacmanCharacter{
+        public abstract class Ghost extends PacmanCharacter{
 
-		
-		private GArc body;
-		//1 is right, 2 is up, 3 is left, and 4 is down
-		private int nextDirection;
-		
-		public Ghost(Color color){
-			super(color);
-			vx = -PacmanConstants.SPEED;
-			vy = 0;
-			setNextDirection(1);
-		}
-		
-		abstract void decideNextDirection(GPacman[] targets);
-		
-		
-		public int getNextDirection() {
-			return nextDirection;
-		}
-
-		public void setNextDirection(int nextDirection) {
-			this.nextDirection = nextDirection;
-		}
-		
-		public Color getColor(){
-			return bodyColor;
-		}
-		
-		public void setColor(Color color){
-			bodyColor = color;
-		}
-		
-		public GArc getBody(){
-			return body;
-		}
-		
-		public void setBody(GArc body){
-			this.body = body;
-		}
-		
-	}
-
-
+                
+                private GArc body;
+                //1 is right, 2 is up, 3 is left, and 4 is down
+                
+                public Ghost(Color color){
+                        super(color);
+                        vx = -PacmanConstants.SPEED;
+                        vy = 0;
+                        setTurnRequest(1);
+                }
+                
+                abstract void decideNextDirection(GPacman[] targets);
+                
+                
+                
+                public Color getColor(){
+                        return bodyColor;
+                }
+                
+                public void setColor(Color color){
+                        bodyColor = color;
+                }
+                
+                public GArc getBody(){
+                        return body;
+                }
+                
+                public void setBody(GArc body){
+                        this.body = body;
+                }
+                
+        }
